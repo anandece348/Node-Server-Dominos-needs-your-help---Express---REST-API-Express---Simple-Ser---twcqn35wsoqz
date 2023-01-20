64ee2ec2bdd5e7fdf889f9e90d3b80d926c25621
anandece348 <anandece2608@gmail.com>
2023-01-21 00:00:12 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
 const express = require("express");
 const app = express();
-// app.use(express.json());
 const obj = {
     phone: '18602100000',
     email: 'guestcaredominos@jublfood.com'
-  }
+}
 app.get("/welcome", (req, res)=>{
-    if(req.url != "/welocme"){
-        return res.status(404).send("Invalid request...")
-    }
-    res.setHeader('Content-Type', 'text/plain');
     res.status(200).send("Welcome to Dominos!");
 })
  
 
 app.get("/contact", (req, res)=>{
-    if(req.url != "/contact"){
-        return res.status(404).send("Invalid request...")
-    }
-    res.setHeader('Content-Type', 'application/json');
     res.status(200).send(obj);
 });
 app.listen(8081, ()=>{
