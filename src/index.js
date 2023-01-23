ee235f17fb4c098db3bcac7914ab6f89f5b9944c
anandece348 <anandece2608@gmail.com>
2023-01-23 20:48:07 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ const obj = {
     email: 'guestcaredominos@jublfood.com'
 }
 app.get("/welcome", (req, res)=>{
-    res.status(200).send("Welcome to Dominos!");
+   return res.status(200).send("Welcome to Dominos!");
 })
  
 
 app.get("/contact", (req, res)=>{
-    res.status(200).send(obj);
+   return res.status(200).send(obj);
 });
 app.listen(8081, ()=>{
     console.log("listening to the port at 8081...");
